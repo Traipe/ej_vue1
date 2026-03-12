c5bd96d0b9afc54533931ceea907adc2bf87820a
jimena <jimenatraipe@gmail.com>
2026-03-12 15:15:17 -0300

    analisis L1 Vue.js

---
+++ L1_vuejs_analisis/TarjetaUsuario.vue
@@ -0,0 +1,29 @@
+<!--Reutilización y modularidad-->
+<template>
+  <div class="tarjeta">
+    <h2>{{ nombre }}</h2>
+    <p>Contador: {{ contador }}</p>
+    <button @click="$emit('reset')">Resetear</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    nombre: String,
+    contador: Number
+  }
+};
+</script>
+
+<style>
+.tarjeta {
+  border: 1px solid #d1d5db;
+  padding: 1rem;
+  border-radius: 8px;
+  background-color: #f3f4f6;
+}
+</style>
+
+<!-- La arquitectura basada en componentes permite reutilizar código, mantenerlo modular y facilitar la escalabilidad-->
+ 
---
+++ L1_vuejs_analisis/app.vue
@@ -0,0 +1,34 @@
+<!--Implementación de un componente básico-->
+<template>
+  <div>
+    <!--{{ nombre }} muestra el dato-->
+    <h1>Holi {{ nombre }}</h1> 
+    <!-- v-model="nombre" permite editarlo desde el imput-->
+    <input v-model="nombre" placeholder="Editar nombre" />
+    <p>Contador: {{ contador }}</p>
+    <button @click="incrementar">Incrementar</button>
+  </div>
+</template>
+
+<script>
+// data es el MODEL, el template es la VIEW, y methods es VIEWMODEL, es decir, la lógica que conecta el modelo con la vista.
+export default {
+  data() {
+    return {
+      nombre: "Jimena",
+      contador: 0
+    };
+  },
+  methods: {
+    incrementar() {
+      this.contador++;
+    }
+  }
+};
+</script>
+
+<style>
+h1 {
+  color: #a78bfa; /* tono pastel */
+}
+</style>
